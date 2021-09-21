0992289db34618e238f87237adf83f92bbc3dd88
YfNightWind <lyh1147557099@outlook.com>
2021-09-21 16:07:44 +0800

    add bubble sort

---
+++ Sorting Algorithms/BubbleSort.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+void BubbleSort(int arr[], int n);
+void InverseBubbleSort(int arr[], int n);
+void printArray(int arr[], int n);
+const int n = 8;
+int main(int argc, char **argv)
+{
+    int a[n] = {6,5,3,1,8,7,2,4};
+    BubbleSort(a,n);
+    cout << "从小到大的冒泡排序:\n";
+    printArray(a,n);
+    int b[n] = {6,5,3,1,8,7,2,4};
+    InverseBubbleSort(b,n);
+    cout << "从大到小的冒泡排序:\n";
+    printArray(b,n);
+    return 0;
+}
+void BubbleSort(int arr[], int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - i - 1; j++)
+        {
+            if(arr[j] > arr[j+1])
+                swap(arr[j],arr[j+1]);
+        }
+    }
+}
+void InverseBubbleSort(int arr[], int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = n - 1; j > 0; j--)
+        {
+            if(arr[j] > arr[j - 1])
+            {
+                swap(arr[j],arr[j-1]);
+            }
+        }
+    }
+}
+void printArray(int arr[], int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
