b15a5a8a75810734c22f7e710de320d72113a96f
YfNightWind <lyh1147557099@outlook.com>
2021-09-22 15:18:30 +0800

    delete useless files

--- Sorting Algorithms/QuickSort.cpp
+++ Sorting Algorithms/QuickSort.cpp
@@ -8,4 +8,16 @@
 #include <iostream>
 using namespace std;
 
+void QuickSort(int nums[]);
 
+int main()
+{
+    
+    return 0;
+}
+
+void QuickSort(int nums[], int n)
+{
+    int temp, min = 0;
+    for(int i = 0; i <= nums )
+}
--- test.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  test.cpp
-//  Algorithm
-//
-//  Created by Alex Lin on 2021/9/22.
-//
-
-#include <stdio.h>
