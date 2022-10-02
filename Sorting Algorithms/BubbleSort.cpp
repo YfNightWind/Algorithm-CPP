262b70834f037a686e9f3f0ed254200c167bfc12
YfNightWind <linyuhong.personal@outlook.com>
2022-10-02 15:48:10 +0800

    🦄 refactor: add flag

--- Sorting Algorithms/BubbleSort.cpp
+++ Sorting Algorithms/BubbleSort.cpp
@@ -1,58 +1,43 @@
-//
-//  BubbleSort.cpp
-//  Algorithm
-//
-//  Created by Alex Lin on 2021/9/21.
-//
-
 #include <iostream>
-
-using namespace std;
 
 const int n = 8;
 
-void BubbleSort(int arr[], int n)
+void swap(int &a, int &b)
 {
-    for(int i = 0; i < n - 1; i++)
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void BubbleSort(int a[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
     {
-        for(int j = 0; j < n - i - 1; j++)
+        bool flag = false;              // 表示本趟冒泡是否发生交换标志
+        for (int j = n - 1; j > i; j--) // 一趟冒泡过程
         {
-            if(arr[j] > arr[j+1])
-                swap(arr[j],arr[j+1]);
+            if (a[j - 1] > a[j])      // 若为逆序
+                swap(a[j - 1], a[j]); // 交换
+            flag = true;
         }
+        if (flag == false)
+            return; // 本趟遍历后没有发生交换，说明表已经有序
     }
 }
-void InverseBubbleSort(int arr[], int n)
-{
-    for(int i = 0; i < n - 1; i++)
-    {
-        for(int j = n - 1; j > 0; j--)
-        {
-            if(arr[j] > arr[j - 1])
-            {
-                swap(arr[j],arr[j-1]);
-            }
-        }
-    }
-}
+
 void printArray(int arr[], int n)
 {
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cout << arr[i] << " ";
+        std::cout << arr[i] << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
 }
 
 int main()
 {
-    int a[n] = {6,5,3,1,8,7,2,4};
-    BubbleSort(a,n);
-    cout << "从小到大的冒泡排序:\n";
-    printArray(a,n);
-    int b[n] = {6,5,3,1,8,7,2,4};
-    InverseBubbleSort(b,n);
-    cout << "从大到小的冒泡排序:\n";
-    printArray(b,n);
+    int a[n] = {6, 5, 3, 1, 8, 7, 2, 4};
+    BubbleSort(a, n);
+    printArray(a, n); // 1 2 3 4 5 6 7 8
     return 0;
 }
